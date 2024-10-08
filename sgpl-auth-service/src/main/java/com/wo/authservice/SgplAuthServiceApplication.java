7d7fa21265591e12d7d0c5ec701925f16aab4eca
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-10-08 14:12:17 -0500

    Add auth-service microservice

---
+++ sgpl-auth-service/src/main/java/com/wo/authservice/SgplAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wo.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgplAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgplAuthServiceApplication.class, args);
+    }
+
+}
---
+++ sgpl-auth-service/src/test/java/com/wo/authservice/SgplAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wo.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgplAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
