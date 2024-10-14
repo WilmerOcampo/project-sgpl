c78f1e68642584142401e4326706a2bba01f007d
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-10-14 16:48:36 -0500

    Updare package ms loan

---
+++ sgpl-loan-service/src/main/java/com/wo/loanservice/services/ILoanService.java
@@ -0,0 +1,28 @@
+package com.wo.loanservice.Service;
+
+import com.wo.loanservice.model.Loan;
+import com.wo.loanservice.model.enums.ELoan;
+import com.wo.loanservice.payload.OperationResponse;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ILoanService {
+
+    List<Loan> getAllLoans();
+
+    List<Loan> findByUserId(Long userId);
+
+    List<Loan> findByBookId(Long bookId);
+
+    List<Loan> findAllByStatus(ELoan status);
+
+    OperationResponse finalize(Long id);
+
+    Optional<Loan> findById(Long id);
+
+    Loan generateLoan(Loan loan);
+
+    List<Loan> findLoansWithExpiredReturnDate();
+
+}
