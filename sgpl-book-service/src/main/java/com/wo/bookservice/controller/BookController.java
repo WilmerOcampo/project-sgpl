429d38e76bba291118db1226092c54378fecf615
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-10-15 13:52:29 -0500

    Refact endpoints api/book api/category

--- sgpl-book-service/src/main/java/com/wo/bookservice/controller/BookController.java
+++ sgpl-book-service/src/main/java/com/wo/bookservice/controller/BookController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/book")
 @RequiredArgsConstructor
 @CrossOrigin
 public class BookController {
